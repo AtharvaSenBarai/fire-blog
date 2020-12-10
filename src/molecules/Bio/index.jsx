229f089ed2830e0782ef893bff8c03ff684d4207
iamharky <harky@yandex.com>
2020-12-10 16:32:43 +0300

    Shortened bio text.

--- src/molecules/Bio/index.jsx
+++ src/molecules/Bio/index.jsx
@@ -10,10 +10,9 @@ const Bio = () => {
       <BioImg />
       <BioText>
         <p>
-          Hi! I am Turgut Yavuz. I am a Front-End Developer. I develop web apps with{' '}
-          <Skill name="React" color="#222" bgColor="#61DAFB" />, <Skill name="Svelte" color="#FFF" bgColor="#FF3E00" />{' '}
-          and <Skill name="Vue" color="#FFF" bgColor="#41B883" />. I develop mobile apps with{' '}
-          <Skill name="React Native" color="#222" bgColor="#61DAFB" />.
+          Hi! I am Turgut Yavuz. I develop web apps with <Skill name="React" color="#222" bgColor="#61DAFB" />,{' '}
+          <Skill name="Svelte" color="#FFF" bgColor="#FF3E00" /> and <Skill name="Vue" color="#FFF" bgColor="#41B883" />
+          . I develop mobile apps with <Skill name="React Native" color="#222" bgColor="#61DAFB" />.
         </p>
       </BioText>
     </div>
