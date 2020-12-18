9f5664a8964c17ca98b583f207c5b54a9dd5a203
iamharky <harky@yandex.com>
2020-12-18 21:30:39 +0300

    Set up tinykeys to access login page by key presses

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,12 @@
 import React, {useState, createContext, useEffect} from 'react';
 import firebase from 'firebase/app';
 import db from './firebase';
+import tinykeys from 'tinykeys';
+import {withRouter} from 'react-router-dom';
 
 export const Context = createContext();
 
-const ContextProvider = ({children}) => {
+const ContextProvider = ({children, history}) => {
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState(null);
 
@@ -23,7 +25,15 @@ const ContextProvider = ({children}) => {
       });
   }, []);
 
+  useEffect(() => {
+    let unsubscribe = tinykeys(window, {
+      'l o g i n': () => history.push('/login'),
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return <Context.Provider value={{user, posts}}>{children}</Context.Provider>;
 };
 
-export default ContextProvider;
+export default withRouter(ContextProvider);
